e85924ed79c94ad2ddb0a2f875fc05ffd3baa1ef
Dirk Moeller <moeller.dirk80@gmail.com>
2019-04-08 21:52:10 +0200

    bla blupp

--- regfile/regfile.v
+++ regfile/regfile.v
@@ -30,7 +30,7 @@ input [3:0] i_alu_l_sel; // which reg to output on alu_l
 input [3:0] i_alu_r_sel; // which reg to output on alu_r
 input [2:0] i_addr_sel; // which reg pair to output on addr bus
 
-reg [7:0] registers[0:`REGCOUNT]; // AB(0,1) CD(2,3) EF(4,5) GH(6,7) SP(8,9) PC(10,11)
+reg [7:0] registers[0:`REGCOUNT-1]; // AB(0,1) CD(2,3) EF(4,5) GH(6,7) SP(8,9) PC(10,11)
 
 assign o_alu_l = i_alu_l_sel < `REGCOUNT ? registers[i_alu_l_sel] : i_dat;
 assign o_alu_r = i_alu_r_sel < `REGCOUNT ? registers[i_alu_r_sel] : i_dat;
