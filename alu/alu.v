afad4f08edb1494e998c6093aaa129327b001014
Dirk Moeller <moeller.dirk80@gmail.com>
2019-04-11 21:58:16 +0200

    alu:
    - testbench
    - shifts

--- alu/alu.v
+++ alu/alu.v
@@ -1,6 +1,4 @@
-module Alu(
-    i_clk,
-
+module alu(
     i_alu_l,
     i_alu_r,
     i_op,
@@ -12,13 +10,12 @@ module Alu(
 `define FLAG_C 1
 `define FLAG_N 2
 
-input i_clk;
 input [7:0] i_alu_l;
 input [7:0] i_alu_r;
 input [2:0] i_op;
 
 output [7:0] o_alu;
-output [3:0] o_flags;
+output [2:0] o_flags;
 
 reg [7:0] r_alu_out;
 reg r_carry;
@@ -29,17 +26,23 @@ assign o_alu = r_alu_out;
 
 parameter
     OP_ADD = 0,
-    OP_AND = 1,
-    OP_OR  = 2,
-    OP_XOR = 3;
+    OP_ADC = 1,
+    OP_AND = 2,
+    OP_OR  = 3,
+    OP_XOR = 4,
+    OP_LSL = 5,
+    OP_LSR = 6;
 
-always @(posedge i_clk)
+always @(*)
 begin
     case(i_op)
         OP_ADD: { r_carry, r_alu_out } = { 1'b0, i_alu_l } + { 1'b0, i_alu_r };
+        OP_ADC: { r_carry, r_alu_out } = { 1'b0, i_alu_l } + { 1'b0, i_alu_r } + { 8'd0, r_carry };
         OP_AND: r_alu_out = i_alu_l & i_alu_r;
         OP_OR:  r_alu_out = i_alu_l | i_alu_r;
         OP_XOR: r_alu_out = i_alu_l ^ i_alu_r;
+        OP_LSL: { r_carry, r_alu_out } = { i_alu_l, 1'b0 } << i_alu_r;
+        OP_LSR: { r_alu_out, r_carry } = { 1'b0, i_alu_l } >> i_alu_r;
     endcase
 end
 
