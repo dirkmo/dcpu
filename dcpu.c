6de09248ec4f6a138a0f55d88d463afad89d06b3
Dirk Moeller <moeller.dirk80@gmail.com>
2021-01-16 20:22:12 +0100

    instruction set

--- dcpu.c
+++ dcpu.c
@@ -12,6 +12,8 @@
 
 # Alu Ops
 
+T <- N op T
+
 1000 0000 plus
 1000 0001 minus
 1000 0010 and
@@ -23,170 +25,87 @@
 
 # Stack
 
-1001 0000 push t
-1001 0001 push a
-1001 0010 push #imm
-1001 0011 push n
-1001 0100 push usp
-1001 0101 push status
-1001 0110
-1001 0111
+1001 0000 push t            ; mem[dsp] <- n, dsp++, n <- t
+1001 0001 push a            ; mem[dsp] <- n, dsp++, n <- t, t <- a
+1001 0010 push n            ; mem[dsp] <- n, dsp++, n <- t, t <- n
+1001 0011 push usp          ; mem[dsp] <- n, dsp++, n <- t, t <- usp
+1001 01xy push #im          ; mem[dsp] <- n, dsp++, n <- t, t <- {x, ir[22:16], y, ir[14:8]}
 
-1001 1000
+1001 1000 push status       ; mem[dsp] <- n, dsp++, n <- t, t <- status
 1001 1001
-1001 1010 pop
-1001 1011 popa
-1001 1100 usp++
-1001 1101 usp--
-1001 1110
-1001 1111
+1001 1010 
+1001 1011 
+1001 1100 pop               ; t <- n, n <- mem[dsp-1], dsp--
+1001 1101 popa              ; a <- mem[asp-1], asp--
+1001 1110 usp++             ; usp++
+1001 1111 usp--             ; usp--
 
 # Memory
 
-1010 0000 fetch t
-1010 0001 fetch a 
-1010 0010 fetch #ofs
-1010 0011 fetch u+#ofs
+1010 0000 fetch t           ; t <- mem[t]
+1010 0001 fetch a           ; t <- mem[a]
+1010 0010 fetch u+#ofs      ; t <- mem[usp+#ofs]
+1010 0011
+1010 01xy fetch #ofs        ; t <- mem[#ofs] mit #ofs = {x, ir[22:16], y, ir[14:8]}
 
-1010 0100 store t
-1010 0101 store a
-1010 0110 store #ofs
-1010 0111 store u+#ofs
+1010 1000 store t           ; mem[t] <- n
+1010 1001 store a           ; mem[a] <- t
+1010 1010 store u+#ofs      ; mem[usp+#ofs] <- t
+1010 1011
+1010 11xy store #ofs        ; mem[#ofs] <- t mit #ofs = {x, ir[22:16], y, ir[14:8]}
 
 # Jumps
-1011 0000 jmp t
-1011 0001 jmp a
-1011 0010 jmp #im
+1011 0000 jmp t             ; pc <- t
+1011 0001 jmp a             ; pc <- a
+1011 0010
+1011 0011
+1011 01xy jmp #im           ; pc <- #im mit #im =  {x, ir[22:16], y, ir[14:8]}
 
-1011 0100 jc t
-1011 0101 jc a
-1011 0110 jc #im
+1011 1000 bra t
+1011 1001 bra a
+1011 1010
+1011 1011
+1011 11xy bra #im           ; #im =  {x, ir[22:16], y, ir[14:8]}
 
-1011 1000 jz t
-1011 1001 jz a
-1011 1010 jz #im
+1100 0000 jc t
+1100 0001 jc a
+1100 0010
+1100 0011
+1100 01xy jc #im           ; #im =  {x, ir[22:16], y, ir[14:8]}
 
-1011 1100 jnz t
-1011 1101 jnz a
-1011 1110 jnz #im
+1100 1000 bc t
+1100 1001 bc a
+1100 1010
+1100 1011
+1100 11xy bc #im
 
-1100 bra t
-1100 bra a
-1100 bra #im
+1101 0000 jz t
+1101 0001 jz a
+1101 0010
+1101 0011
+1101 01xy jz #im
 
-1100 bc t
-1100 bc a
-1100 bc #im
+1101 1000 bz t
+1101 1001 bz a
+1101 1010
+1101 1011
+1101 11xy bz #im
 
-1100 bz t
-1100 bz a
-1100 bz #im
+1110 0000 jnz t
+1110 0001 jnz a
+1110 0010
+1110 0011
+1110 01xy jnz #im
 
-1100 bnz t
-1100 bnz a
-1100 bnz #im
+1110 1000 bnz t
+1110 1001 bnz a
+1110 1010
+1110 1011
+1110 11xy bnz #im
 
 # Misc
-1111 0000 status
-1111 1000 int
-
-
-
-pop      pop ds
-    dsp--
-    t <- n
-    n <- mem[dsp]
-
-popa     pop as
-    asp--
-    a <- mem[asp]
-
-upd     decrease usp
-    usp--
-
-upi     increase usp
-    usp++
-
-jmp im      jump to immediate address
-    pc <- ir[]
-
-jpz im      if zero, jump to immediate address
-    pc <- zero ? IR : pc+1
-
-jpc im      if carry, jump to immediate address
-    pc <- carry ? IR : pc+1
-
-jmp a       jump to a
-    pc <- a
-
-jmp t       jump to t
-    pc <- t
-
-bra, braz, brac
-    mem[asp] <- a, asp++
-    a <- pc + 1
-    pc <- [im, a, t]
-
-AND         logical and
-    T <- T & N
-
-int         jump to interrupt vector address
-    pc <- INT-ADDR
-
-push #imm   
-    mem[dsp] <- n
-    dsp++
-    n <- t
-    T <- ir[]
-
-push t
-    mem[dsp] <- n, dsp++
-    n <- t
-
-push n
-    mem[dsp] <- n, dsp++
-    n <- t, t <- n
-
-push a
-    mem[dsp] <- n, dsp++
-    n <- t
-    t <- a
-
-push usp
-    mem[dsp] <- n, dsp++
-    n <- t
-    t <- usp
-
-push status
-    mem[dsp] <- n, dsp++
-    n <- t
-    t <- status
-
-
-fetch t   load from memory with address in t or a
-    t <- mem[t]
-
-fetch a
-    t <- mem[a]
-
-fetch #ofs  load from memory, address in ir[]
-    t <- mem[#ofs]
-
-fetch u+#ofs  load from usp+ofs (ofs in ir[])
-    t <- mem[usp+ofs]
-
-store n     store n to memory with address in t
-    mem[t] <- n
-
-store t     store t to memory with address in a
-    mem[a] <- t
-
-store #ofs  store t to memory address in ir[]
-    mem[#ofs] <- t
-
-store u+#ofs  store t to memory with address usp+ofs (ofs in ir[])
-    mem[usp+ofs] <- t
-
+1111 0000 status                ; status <- t
+1111 1000 int                   ; pc <- int-addr
 
 */
 
@@ -217,7 +136,7 @@ typedef struct {
 } cpu_t;
 
 void reset(cpu_t *cpu) {
-    
+
 }
 
 
