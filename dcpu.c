64b4f114459973b181cef70e6b00e6418572d4a8
Dirk Moeller <moeller.dirk80@gmail.com>
2021-01-17 14:22:43 +0100

    basic implementation done

--- dcpu.c
+++ dcpu.c
@@ -4,9 +4,9 @@
 #include <assert.h>
 #include <string.h>
 
+#include "dcpu.h"
 
-
-
+#define ARRCOUNT(a) (sizeof(a) / sizeof((a)[0]))
 
 typedef enum {
     ST_RESET = 0,
@@ -14,45 +14,277 @@ typedef enum {
     ST_EXECUTE = 2,
 } state_t;
 
-typedef struct {
-    uint16_t ir[2];
+typedef struct cpu_t cpu_t;
+
+struct cpu_t {
+    
+    // instruction + immediate registers
+    uint8_t ir[3];
+    
+    // program counter, byte addresses
     uint16_t pc;
+    
     // data stack
     uint16_t t;
     uint16_t n;
-    uint16_t dsp;
+    uint16_t dsp; // byte address
+    
     // address stack
-    uint16_t asp;
+    uint16_t asp; // byte address
     uint16_t a;
-    // misc
-    uint16_t usp;
+    
+    // user stack pointer
+    uint16_t usp; // byte address
+
+    // alu
     uint16_t alu_output;
-    uint16_t *bus;
-    uint16_t busaddr;
     uint16_t status; // incl. carry
+
+    // memory access
+    uint16_t* (*bus)(cpu_t *cpu);
+    uint16_t busaddr; // byte address
+
+    // state machine state
     state_t state;
-} cpu_t;
+};
+
+
+void alu(cpu_t *cpu) {
+    int op = cpu->ir[0];
+    uint32_t r = 0;
+    switch(op) {
+        case OP_ADD: r = cpu->n  + cpu->t; break;
+        case OP_SUB: r = cpu->n  - cpu->t; break;
+        case OP_AND: r = cpu->n  & cpu->t; break;
+        case OP_OR:  r = cpu->n  | cpu->t; break;
+        case OP_XOR: r = cpu->n  ^ cpu->t; break;
+        case OP_LSR: r = cpu->n >> cpu->t; break;
+        default:
+            printf("undefined alu op (%02X)\n", op);
+            assert(0);
+    }
+    cpu->alu_output = r & 0xFFFF;
+    cpu->status = (cpu->status & ~FLAG_CARRY) | ((r > 0xFFFF) ? FLAG_CARRY : 0);
+    cpu->status = (cpu->status & ~FLAG_ZERO) | ((cpu->alu_output == 0) ? FLAG_ZERO : 0);
+}
 
 void reset(cpu_t *cpu) {
-
+    cpu->ir[0] = cpu->ir[1] = cpu->ir[2] = 0;
+    cpu->pc = ADDR_RESET;
+    cpu->a = cpu->t = cpu->n = 0;
+    cpu->usp = cpu->asp = cpu->dsp = 0;
+    cpu->alu_output = 0;
+    cpu->status = 0;
+    cpu->busaddr = 0;
+    cpu->state = ST_RESET;
+    // not setting cpu->bus
 }
 
+uint16_t imm(cpu_t *cpu) {
+    const uint16_t hi = cpu->ir[1] | ((cpu->ir[0] & 2) << 6);
+    const uint16_t lo = cpu->ir[2] | ((cpu->ir[0] & 1) << 7);
+    return lo | (hi << 8);;
+}
+
+void execute_stackop(cpu_t *cpu) {
+    const uint16_t _im = imm(cpu);
+    const uint16_t src[] = { cpu->t, cpu->a, cpu->n, cpu->usp, _im, _im, _im, _im, cpu->status, cpu->dsp, cpu->asp, cpu->pc };
+    const uint8_t idx = cpu->ir[0] & 7;
+    if( idx >= ARRCOUNT(src)) {
+        printf("push: index out of range (%d)\n", idx);
+        assert(0);
+    }
+    // push n to ds
+    cpu->busaddr = cpu->dsp;
+    *cpu->bus(cpu) = src[idx];
+    // inc dsp
+    cpu->dsp++;
+    // put t to n
+    cpu->n = cpu->t;
+    // set t to new value from src
+    cpu->t = src[idx];
+}
+
+void execute_fetchop(cpu_t *cpu) {
+    const uint16_t _im = imm(cpu);
+    const uint16_t uspofs = cpu->usp + imm(cpu);
+    const uint16_t addr[] = { cpu->t, cpu->a, uspofs, 0, _im, _im, _im, _im };
+    const uint8_t idx = cpu->ir[0] & 7;
+    if( idx >= ARRCOUNT(addr) || (idx == 3) ) {
+        printf("fetch: invalid index (%d)\n", idx);
+        assert(0);
+    }
+    // set memory source addr
+    cpu->busaddr = addr[idx];
+    // read from memory into t
+    cpu->t = *cpu->bus(cpu);
+}
+
+void execute_storeop(cpu_t *cpu) {
+    const uint8_t op = cpu->ir[0];
+    const uint16_t _im = imm(cpu);
+    const uint16_t uspofs = cpu->usp + imm(cpu);
+    const uint16_t destaddr[] = { cpu->t, cpu->a, uspofs, 0, _im, _im, _im, _im };
+    const uint8_t idx = cpu->ir[0] & 7;
+    const uint16_t src[] = { cpu->n, cpu->t};
+    if( idx >= ARRCOUNT(destaddr) || (idx == 3) ) {
+        printf("store: invalid index (%d)\n", idx);
+        assert(0);
+    }
+    // set memory dest addr
+    cpu->busaddr = destaddr[idx];
+    // store
+    *cpu->bus(cpu) = (op == OP_STORET) ? src[0] : src[1];
+}
+
+uint16_t jmpaddr(cpu_t *cpu) {
+    const uint16_t _im = imm(cpu);
+    const uint16_t addr[] = { cpu->t, cpu->a, ADDR_INT, 0, _im, _im, _im, _im };
+    const uint8_t idx = cpu->ir[0] & 7;
+    if( idx >= ARRCOUNT(addr) || (idx == 3) ) {
+        printf("store: invalid index (%d)\n", idx);
+        assert(0);
+    }
+    return addr[idx];
+}
+
+void execute_jump(cpu_t *cpu) {
+    cpu->pc = jmpaddr(cpu);
+}
+
+void execute_branch(cpu_t *cpu) {
+    // save a to as
+    cpu->busaddr = cpu->asp;
+    *cpu->bus(cpu) = cpu->a;
+    cpu->asp++;
+    // save pc+1 to a
+    cpu->a = cpu->pc; // pc is already incremented
+    cpu->pc = jmpaddr(cpu);
+}
+
+void execute_pop(cpu_t *cpu) {
+    const uint8_t op = cpu->ir[0];
+    switch(op) {
+        case OP_POP:
+            cpu->dsp--;
+            cpu->t = cpu->n;
+            cpu->busaddr = cpu->dsp;
+            cpu->n = *cpu->bus(cpu);
+            break;
+        case OP_RET:
+            cpu->pc = cpu->a;
+            // intended fallthrough
+        case OP_POPA:
+            cpu->asp--;
+            cpu->busaddr = cpu->asp;
+            cpu->a = *cpu->bus(cpu);
+            break;
+        default:
+            printf("Invalid opcode (%02X)\n", op);
+            assert(0);
+    }
+}
+
+void execute(cpu_t *cpu) {
+    const uint8_t op = cpu->ir[0];
+    const uint8_t opgroup = cpu->ir[0] & OP_MASK;
+    switch(opgroup) {
+        case OP_STACKGROUP1:
+        case OP_STACKGROUP2:
+            execute_stackop(cpu);
+            break;
+        case OP_FETCHGROUP:
+            execute_fetchop(cpu);
+            break;
+        case OP_STOREGROUP:
+            execute_storeop(cpu);
+            break;
+        case OP_JMPGROUP:
+            execute_jump(cpu);
+            break;
+        case OP_BRANCHGROUP:
+            execute_branch(cpu);
+            break;
+        case OP_JMPZGROUP:
+            if (cpu->t == 0) {
+                execute_jump(cpu);
+            }
+            break;
+        case OP_JMPNZGROUP:
+            if (cpu->t != 0) {
+                execute_jump(cpu);
+            }
+            break;
+        case OP_JMPCGROUP:
+            if ((cpu->status & FLAG_CARRY) != 0) {
+                execute_jump(cpu);
+            }
+            break;
+        case OP_JMPNCGROUP:
+            if ((cpu->status & FLAG_CARRY) == 0) {
+                execute_jump(cpu);
+            }
+            break;
+        case OP_POPGROUP:
+            execute_pop(cpu);
+            break;
+        case OP_MISCGROUP:
+            if (op == OP_SETSTATUS) {
+                cpu->status = cpu->t;
+            }
+            break;
+        default: printf("unknown opcode %02X\n", op);
+    }
+}
 
 void statemachine(cpu_t *cpu) {
-
+    switch(cpu->state) {
+        case ST_RESET:
+            reset(cpu);
+            cpu->state = ST_FETCH;
+            break;
+        case ST_FETCH:
+            cpu->busaddr = cpu->pc;
+            cpu->ir[2] = cpu->ir[1];
+            cpu->ir[1] = cpu->ir[0];
+            cpu->ir[0] = (cpu->pc&1) ? (*cpu->bus(cpu) >> 8 ): *cpu->bus(cpu);
+            cpu->state = (cpu->ir[0] & OP_IMM_MASK) ? ST_EXECUTE : ST_FETCH;
+            cpu->pc++; // first increase pc. pc might again be overwritten by jumps
+            break;
+        case ST_EXECUTE:
+            execute(cpu);
+            // finally clear ir[]
+            cpu->ir[1] = cpu->ir[0] = 0;
+            cpu->state = ST_FETCH;
+            break;
+        default:;
+    }
 }
 
 
-static uint16_t mem[0x10000];
+static uint8_t mem[0x1000];
+
+uint16_t *bus(cpu_t *cpu) {
+    assert(cpu->busaddr < sizeof(mem));
+    return (uint16_t*)&mem[cpu->busaddr & 0xFFFe];
+}
+
 
 int main(int argc, char *argv[]) {
     cpu_t cpu;
+    cpu.bus = bus;
+
     reset(&cpu);
 
-    uint16_t prog[] = { };
-    memcpy(mem, prog, sizeof(prog));
+    uint8_t prog[] = { 0x7e, 0x7f, OP_PUSHI, OP_END };
+    
+    for (int i = 0; i < sizeof(prog); i+=2) {
+        cpu.busaddr = ADDR_RESET + i;
+        *bus(&cpu) = prog[i] | (prog[i+1] << 8);
+    }
+
     int count = 0;
-    while(count++ < 10) {
+    while(count++ < 10 && cpu.ir[0] != OP_END) {
         statemachine(&cpu);
     }
     return 0;
--- dcpu.h
+++ dcpu.h
@@ -1,17 +1,27 @@
 #ifndef _DCPU_H
 #define _DCPU_H
 
+#define FLAG_ZERO  0
+#define FLAG_CARRY 1
+#define FLAG_INTEN 2
+
+#define ADDR_RESET 0x0100
+#define ADDR_INT   0xFFF0
+
+// ## memory accesses are little endian:
+// addr 0: low byte
+// addr 1: high byte
 
 // ## Instruction set
 
 typedef enum {
+    OP_MASK     = 0xF8,
 
     // Immediate instruction pushes data into ir
     // 0IIIIIII   ir[23:0] <= {ir[15:0], 0iiiiiii}
-
     // after every non-immediate instruction, ir[23:0] will be cleared.
-
-    OP_MASK     = 0xF8,
+    OP_IMM_MASK = 0x80,
+    
 
     // # Alu Ops    T <- N op T
     OP_ALU      = 0x80,           // 1000 0xxx
@@ -29,16 +39,16 @@ typedef enum {
     OP_PUSHT        = OP_STACKGROUP1 | 0x0,  // 1001 0000 push t            ; mem[dsp] <- n, dsp++, n <- t
     OP_PUSHA        = OP_STACKGROUP1 | 0x1,  // 1001 0001 push a            ; mem[dsp] <- n, dsp++, n <- t, t <- a
     OP_PUSHN        = OP_STACKGROUP1 | 0x2,  // 1001 0010 push n            ; mem[dsp] <- n, dsp++, n <- t, t <- n
-    OP_PUSHU        = OP_STACKGROUP1 | 0x3,  // 1001 0011 push usp          ; mem[dsp] <- n, dsp++, n <- t, t <- usp
+    OP_PUSHUSP      = OP_STACKGROUP1 | 0x3,  // 1001 0011 push usp          ; mem[dsp] <- n, dsp++, n <- t, t <- usp
     OP_PUSHI        = OP_STACKGROUP1 | 0x4,  // 1001 01xy push #im          ; mem[dsp] <- n, dsp++, n <- t, t <- {x, ir[22:16], y, ir[14:8]}
 
     OP_STACKGROUP2  = 0x98,
     OP_PUSHS        = OP_STACKGROUP2 | 0x8, // 1001 1000 push status       ; mem[dsp] <- n, dsp++, n <- t, t <- status
-    OP_PUSHD        = OP_STACKGROUP2 | 0x9, // 1001 1001 push dsp          ; mem[dsp] <- n, dsp++, n <- t, t <- dsp
-    OP_PUSHA        = OP_STACKGROUP2 | 0xA, // 1001 1010 push asp          ; mem[dsp] <- n, dsp++, n <- t, t <- asp
-    // OP_PUSHx     = OP_STACKGROUP2 | 0xB, // 1001 1011 
-    OP_POP          = OP_STACKGROUP2 | 0xC, // 1001 1100 pop               ; t <- n, n <- mem[dsp-1], dsp--
-    OP_POPA         = OP_STACKGROUP2 | 0xD, // 1001 1101 popa              ; a <- mem[asp-1], asp--
+    OP_PUSHDSP      = OP_STACKGROUP2 | 0x9, // 1001 1001 push dsp          ; mem[dsp] <- n, dsp++, n <- t, t <- dsp
+    OP_PUSHASP      = OP_STACKGROUP2 | 0xA, // 1001 1010 push asp          ; mem[dsp] <- n, dsp++, n <- t, t <- asp
+    OP_PUSHPC       = OP_STACKGROUP2 | 0xB, // 1001 1011 push pc+1         ; mem[dsp] <- n, dsp++, n <- t, t <- pc+1
+    // OP_PUSHx     = OP_STACKGROUP2 | 0xC, // 1001 1100 
+    // OP_PUSHx     = OP_STACKGROUP2 | 0xD, // 1001 1101 
     // OP_PUSHx     = OP_STACKGROUP2 | 0xE, // 1001 1110
     // OP_PUSHx     = OP_STACKGROUP2 | 0xF, // 1001 1111 
 
@@ -68,36 +78,51 @@ typedef enum {
     OP_BRANCHGROUP  = 0xB8,
     OP_BRAT         = OP_BRANCHGROUP | 0x0, // 1011 1000 bra t
     OP_BRAA         = OP_BRANCHGROUP | 0x1, // 1011 1001 bra a
-    // OP_BRA       = OP_BRANCHGROUP | 0x2, // 1011 1010
-    // OP_BRA       = OP_BRANCHGROUP | 0x3, // 1011 1011
+    OP_INT          = OP_BRANCHGROUP | 0x2, // 1011 1010 int               ; mem[asp] <- a, a <- pc+1, asp++, pc <- int-vec
+    // OP_BRAx      = OP_BRANCHGROUP | 0x3, // 1011 1011
     OP_BRAABS       = OP_BRANCHGROUP | 0x4, // 1011 11xy bra #im           ; #im =  {x, ir[22:16], y, ir[14:8]}
 
-    OP_JMPZGROUP   = 0xC0,
+    OP_JMPZGROUP    = 0xC0,
     OP_JMPZT        = OP_JMPZGROUP | 0x0, // 1100 0000 jz t
     OP_JMPZA        = OP_JMPZGROUP | 0x1, // 1100 0001 jz a
     // OP_JMPZ      = OP_JMPZGROUP | 0x2, // 1100 0010
     // OP_JMPZ      = OP_JMPZGROUP | 0x3, // 1100 0011
     OP_JMPZABS      = OP_JMPZGROUP | 0x4, // 1100 01xy jz #im
 
-    OP_JMPNZGROUP  = 0xC8,
-    OP_JMPNZT        = OP_JMPNZGROUP | 000, // 1100 1000 jnz t
-    OP_JMPNZA        = OP_JMPNZGROUP | 001, // 1100 1001 jnz a
-    // OP_JMPNZ      = OP_JMPNZGROUP | 002, // 1100 1010
-    // OP_JMPNZ      = OP_JMPNZGROUP | 003, // 1100 1011
-    OP_JMPNZABS      = OP_JMPNZGROUP | 004, // 1100 11xy jnz #im
+    OP_JMPNZGROUP   = 0xC8,
+    OP_JMPNZT       = OP_JMPNZGROUP | 000, // 1100 1000 jnz t
+    OP_JMPNZA       = OP_JMPNZGROUP | 001, // 1100 1001 jnz a
+    // OP_JMPNZ     = OP_JMPNZGROUP | 002, // 1100 1010
+    // OP_JMPNZ     = OP_JMPNZGROUP | 003, // 1100 1011
+    OP_JMPNZABS     = OP_JMPNZGROUP | 004, // 1100 11xy jnz #im
 
-    OP_JMPCGROUP     = 0xD0,
-    OP_JMPCT         = OP_JMPCGROUP | 0x0, // 1101 0000 jc t
-    OP_JMPCA         = OP_JMPCGROUP | 0x1, // 1101 0001 jc a
-    // OP_JMPC       = OP_JMPCGROUP | 0x2, // 1101 0010
-    // OP_JMPC       = OP_JMPCGROUP | 0x3, // 1101 0011
-    OP_JMPCABS       = OP_JMPCGROUP | 0x4, // 1101 01xy jc #im           ; #im =  {x, ir[22:16], y, ir[14:8]}
+    OP_JMPCGROUP    = 0xD0,
+    OP_JMPCT        = OP_JMPCGROUP | 0x0, // 1101 0000 jc t
+    OP_JMPCA        = OP_JMPCGROUP | 0x1, // 1101 0001 jc a
+    // OP_JMPC      = OP_JMPCGROUP | 0x2, // 1101 0010
+    // OP_JMPC      = OP_JMPCGROUP | 0x3, // 1101 0011
+    OP_JMPCABS      = OP_JMPCGROUP | 0x4, // 1101 01xy jc #im           ; #im =  {x, ir[22:16], y, ir[14:8]}
+
+    OP_JMPNCGROUP   = 0xD8,
+    OP_JMPNCT       = OP_JMPNCGROUP | 0x0, // 1101 1000 jc t
+    OP_JMPNCA       = OP_JMPNCGROUP | 0x1, // 1101 1001 jc a
+    // OP_JMPNC     = OP_JMPNCGROUP | 0x2, // 1101 1010
+    // OP_JMPNC     = OP_JMPNCGROUP | 0x3, // 1101 1011
+    OP_JMPNCABS     = OP_JMPNCGROUP | 0x4, // 1101 11xy jc #im           ; #im =  {x, ir[22:16], y, ir[14:8]}
+
+    OP_POPGROUP     = 0xE0,
+    OP_POP          = OP_POPGROUP | 0x0, // 1110 0000 pop       ; t <- n, n <- mem[dsp-1], dsp--
+    // OP_POPx      = OP_POPGROUP | 0x1, // 1110 0001 
+    OP_POPA         = OP_POPGROUP | 0x2, // 1110 0010 popa      ; a <- mem[asp-1], asp--
+    OP_RET          = OP_POPGROUP | 0x3, // 1110 0011 ret       ; pc <- a, a <- mem[asp-1], asp--
 
     // # Misc
-    OP_MISCGROUP     = 0xF0,
-    OP_SETSTATUS     = OP_MISCGROUP | 0x0, // 1111 0000 status                ; status <- t
-    OP_INT           = OP_MISCGROUP | 0x8// 1111 1000 int                   ; pc <- int-addr
+    OP_MISCGROUP    = 0xF0,
+    OP_SETSTATUS    = OP_MISCGROUP | 0x0, // 1111 0000 status                ; status <- t
 
+
+
+    OP_END = 0xFF
 } opcode_t;
 
 #endif
