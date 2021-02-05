6408fb4aebc89ca402f507330c29055c3ba3221f
Dirk Moeller <moeller.dirk80@gmail.com>
2021-02-05 22:32:10 +0100

    loadhex sim

--- cmodel/sim.c
+++ cmodel/sim.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
+#include <ctype.h>
 #include "dcpu.h"
 
 static uint8_t mem[0x10000];
@@ -17,39 +18,72 @@ void copy(cpu_t *cpu, uint8_t *prog, int len) {
     }
 }
 
-bool test(cpu_t *cpu, uint8_t *prog, int len, cpu_t *res) {
-    copy(cpu, prog, len);
-    reset(cpu);
-    int count = 0;
-    while(count++ < 100 && cpu->ir[0] != OP_END) {
-        statemachine(cpu);
+uint16_t convertHexnumber(const char *s, int len) {
+    uint16_t num = 0;
+    for (int i = 0; i < len; i++) {
+        char c = toupper(s[i]);
+        if (c >= '0' && c <= '9') {
+            c -= '0';
+        } else if (c >= 'A' && c <= 'F') {
+            c -= 'A' - 10;
+        }
+        num = (num << 4) | c;
     }
-    bool t = cpu->t == res->t;
-    if (!t) printf("t wrong (%04X)\n", cpu->t);
-    bool n = cpu->n == res->n;
-    if (!n) printf("n wrong (%04X)\n", cpu->n);
-    bool a = cpu->a == res->a;
-    if (!a) printf("a wrong (%04X)\n", cpu->a);
-    bool dsp = cpu->dsp == res->dsp;
-    if (!dsp) printf("dsp wrong (%04X)\n", cpu->dsp);
-    bool asp = cpu->asp == res->asp;
-    if (!asp) printf("asp wrong (%04X)\n", cpu->asp);
-    bool usp = cpu->usp == res->usp;
-    if (!usp) printf("usp wrong (%04X)\n", cpu->usp);
-    bool pc = cpu->pc == res->pc;
-    if (!pc) printf("pc wrong (%04X)\n", cpu->pc);
-    return t & n & a & dsp & asp & usp & pc;
+    return num;
+}
+
+int loadHex(const char *fn, uint8_t *mem) {
+    //  Hexfile format:
+    //  : COUNT ADDRESS TYPE DATA CHKSUM
+    char line[300];
+    FILE *f = fopen(fn, "r");
+    if (f == NULL) {
+        return 3;
+    }
+    while (!feof(f)) {
+        if( fgets(line, sizeof(line), f) == 0) {
+            break;
+        }
+        if (line[0] != ':') {
+            return 1;
+        }
+        uint8_t len = convertHexnumber(line+1, 2);
+        uint16_t addr = convertHexnumber(line+3, 4);
+        uint8_t type = convertHexnumber(line+7, 2);
+        uint8_t chksum = convertHexnumber(line+9+len*2, 2);
+        printf("%04x: ", addr);
+        for ( int i = 0; i<len; i++) {
+            uint8_t dat = convertHexnumber(line+9+i*2, 2);
+            mem[addr+i] = dat;
+            chksum += dat;
+            printf("%02x ", dat);
+        }
+        chksum += len + type + (addr & 0xff) + ((addr >> 8) & 0xff);
+        if (chksum) {
+            return 2;
+        }
+        printf("\n");
+    }
+    fclose(f);
+    return 0;
 }
 
 int main(int argc, char *argv[]) {
-    cpu_t cpu, res;
+    if (argc < 2) {
+        printf("Usage: sim <hex-file>\n");
+        return 1;
+    }
+    cpu_t cpu;
     cpu.bus = bus;
 
-    int fail = 0;
     reset(&cpu);
 
-    // copy(&cpu, prog, len);
-    // reset(cpu);
+    if (loadHex(argv[1], mem)) {
+        printf("ERROR: Failed to load hex-file %s\n",argv[1]);
+        return 2;
+    }
+
+
     // int count = 0;
     // while(count++ < 100 && cpu.ir[0] != OP_END) {
     //     statemachine(&cpu);
