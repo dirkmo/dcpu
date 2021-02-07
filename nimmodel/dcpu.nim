5fb3e86935b9f77a4fa59b67694276d2224c306f
Dirk Moeller <moeller.dirk80@gmail.com>
2021-02-07 12:08:17 +0100

    nim model

--- nimmodel/dcpu.nim
+++ nimmodel/dcpu.nim
@@ -1,17 +1,40 @@
 import strformat
 import opcodes
 
+const
+    ADDR_INT*   = 0xfff0
+    ADDR_RESET* = 0x0100
+
 type
     DcpuState = enum
         dsReset, dsFetch, dsExecute
 
-    Dcpu = object
-        ir: array[3,uint8]
+    Dcpu* = object
+        ir: array[0..2,uint8]
         pc: uint16
         t, n, a: uint16
         dsp, asp, usp: uint16
         status: uint16
 
         busaddr: uint16
+        read: proc(): uint16
+        write: proc(dat: uint16)
         state: DcpuState
 
+
+proc reset*(cpu: var Dcpu) =
+    cpu.ir = [0u8, 0u8, 0u8]
+    cpu.pc = ADDR_RESET
+    cpu.t = 0
+    cpu.a = 0
+    cpu.n = 0
+    cpu.dsp = 0
+    cpu.asp = 0
+    cpu.usp = 0
+    cpu.status = 0
+    cpu.state = dsReset
+    # deliberately not setting read/write
+
+proc statemachine*(cpu: var Dcpu) =
+    while true:
+        discard
--- nimmodel/sim.nim
+++ nimmodel/sim.nim
@@ -6,6 +6,11 @@ import
 var mem: array[0..0xffff, uint16]
 
 let params = commandLineParams()
+
+if params.len() < 1:
+    echo "Usage: sim <hexfile>"
+    quit(1)
+
 let hexfn = params[0]
 
 echo fmt"Reading {hexfn}"
@@ -15,4 +20,7 @@ if not success:
     echo fmt"Failed to read {hexfn}"
     quit(1)
 
-var cpu = Dcpu
+var cpu = new Dcpu
+
+cpu.reset()
+
