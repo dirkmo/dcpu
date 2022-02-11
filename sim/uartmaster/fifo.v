68a56c904db9b137aae9d85d7e43a8a17e8b01d5
Dirk Moeller <moeller.dirk80@gmail.com>
2022-02-11 21:22:23 +0100

    fixed fifo bug

--- sim/uartmaster/fifo.v
+++ sim/uartmaster/fifo.v
@@ -4,7 +4,7 @@ module fifo(
     input i_clk,
     input i_reset,
     input  [DW-1:0] i_dat,
-    output [DW-1:0] o_dat,
+    output reg [DW-1:0] o_dat,
     input i_push,
     input i_pop,
     output o_empty,
@@ -22,19 +22,23 @@ reg empty_n;
 
 assign o_empty = ~empty_n;
 assign o_full = ( wr_idx == rd_idx ) && ~o_empty;
-assign o_dat = buffer[rd_idx];
 
 wire [DEPTH-1:0] rd_idx_next = rd_idx + 'd1;
 wire [DEPTH-1:0] wr_idx_next = wr_idx + 'd1;
 
 always @(posedge i_clk) begin
+    o_dat <= buffer[rd_idx];
     if( i_push && ~o_full ) begin
         wr_idx <= wr_idx_next;
         buffer[wr_idx] <= i_dat;
         empty_n <= 1'b1;
-    end else if( i_pop && ~o_empty ) begin
+    end
+    if( i_pop && ~o_empty ) begin
         rd_idx <= rd_idx_next;
-        empty_n <= (wr_idx != rd_idx_next);
+        if (~i_push) begin
+            empty_n <= (wr_idx != rd_idx_next);
+        end
+        o_dat <= buffer[rd_idx+1];
     end
     if( i_reset ) begin
         rd_idx <= 'd0;
