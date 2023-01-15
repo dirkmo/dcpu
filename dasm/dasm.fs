7ecf8105ed515eb15bc829bbf31eaa601f42b294
Dirk Moeller <moeller.dirk80@gmail.com>
2023-01-15 21:35:12 +0100

    wip: dcpu asm in forth

---
+++ dasm/dasm.fs
@@ -0,0 +1,130 @@
+( Forth assembler )
+( How Forth assemblers work: https://www.bradrodriguez.com/papers/tcjassem.txt )
+( Forth crossassembler for J1: https://github.com/jamesbowman/j1 )
+
+
+\ Program will be assembled into tmemory
+0x10000 allocate throw constant tmemory
+tmemory 0x10000 0 fill
+
+: tmemory-dump tmemory there dump ;
+: update s" dasm.fs" included ;
+
+
+\ the dictionary pointer (offset into tmemory)
+variable tdp 0 tdp !
+
+\ here of tmemory (offset into tmemory)
+: there tdp @ ;
+
+\ tc! ( c offs -- )
+\ store c at offset in tmemory
+: tc! tmemory + c! ;
+
+\ tc, ( c -- )
+\ store c in tmemory and inc tdp
+: tc, there tc! 1 tdp +! ;
+
+\ tw! ( w offs -- )
+\ store word (16-bits)
+: tw!
+    2dup swap 8 rshift swap
+    tc! 1+
+    tc!
+    ;
+
+\ tw, ( w -- )
+\ store w in tmemory and inc tdp
+: tw, there tw! 2 tdp +! ;
+
+
+
+
+( mnemonics )
+
+\ call      0 <addr:15>
+: call,     dup 0x8000 and throw tw, ;
+
+\ lit.l     100 <imm:13>
+: lit.l,    dup 0xe000 and throw ( imm out of range)
+            0x8000 or tw, ;
+
+\ lit.h     101 <unused:4> <ret:1> <imm:8>
+\ TODO: [ret] not supported yet
+: lit.h,    dup 0xff00 and throw
+            0xa000 or tw, ;
+
+: lit,      dup 0x1fff and lit.l,
+            dup 0xff > if 8 rshift lit.h, else drop then ;
+
+\ alu       110 <unused:1> <alu:5> <ret:1> <dst:2> <dsp:2> <rsp:2>
+
+0x001 constant R+
+0x002 constant R-
+0x003 constant RCALL
+0x004 constant D+
+0x008 constant D-
+
+0x000 constant DT ( dst T)
+0x100 constant DR ( dst R)
+0x200 constant DP ( dst PC)
+0x300 constant DM ( dst MEM)
+0x400 constant RET
+
+: alu-op ( op -- )
+    create ,
+    does> @ 0xc000 or or or or or tw, ;
+
+0   7 lshift alu-op     A:T,
+1   7 lshift alu-op     A:N,
+2   7 lshift alu-op     A:R,
+3   7 lshift alu-op     A:MEMT,
+4   7 lshift alu-op     A:ADD,
+5   7 lshift alu-op     A:SUB,
+6   7 lshift alu-op     A:NOP,
+7   7 lshift alu-op     A:AND,
+8   7 lshift alu-op     A:OR,
+9   7 lshift alu-op     A:XOR,
+10  7 lshift alu-op     A:LTS,
+11  7 lshift alu-op     A:LT,
+12  7 lshift alu-op     A:SR,
+13  7 lshift alu-op     A:SRW,
+14  7 lshift alu-op     A:SL,
+15  7 lshift alu-op     A:SLW,
+16  7 lshift alu-op     A:JZ,
+17  7 lshift alu-op     A:JNZ,
+18  7 lshift alu-op     A:CARRY,
+19  7 lshift alu-op     A:INV,
+20  7 lshift alu-op     A:MULL,
+21  7 lshift alu-op     A:MULH,
+
+
+\ rjp       111 <cond:3> <imm:10>
+
+0x000 constant rjp-always
+0x400 constant rjp-zero
+0x500 constant rjp-notzero
+0x600 constant rjp-negative
+0x700 constant rjp-notnegative
+
+\ rjp-op, ( cond addr -- )
+\ : rjp-op,   dup 0xfc00 and throw 0xe000 or or ;
+
+: rjp-op
+    create , ( cond -- )
+    does> @ 0xe000 or or tw, ( addr -- )
+    ;
+
+rjp-always          rjp-op      rj,
+rjp-zero            rjp-op      rjz,
+rjp-notzero         rjp-op      rjnz,
+rjp-negative        rjp-op      rjn,
+rjp-notnegative     rjp-op      rjnn,
+
+
+( Labels )
+
+: label
+    create there ,
+    does> @
+    ;
