c07bfa55337cb802bc387d1e670e9501b417f615
Dirk Moeller <moeller.dirk80@gmail.com>
2023-08-22 21:58:37 +0200

    dev

--- colorforth/dcpu.py
+++ colorforth/dcpu.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python3
 
+import sys
+
 class DcpuMemoryIf:
     def read(self, wordaddr) -> int:
         ...
@@ -8,6 +10,7 @@ class DcpuMemoryIf:
         ...
 
 class Dcpu:
+    ALU_OP_MNEMONIC = ["T", "N", "R", "MEMT", "ADD", "SUB", "NOP", "AND", "OR", "XOR", "LTS", "LT", "SR", "SRW", "SL", "SLW", "JZ", "JNZ", "CARRY", "INV", "MULL", "MULH"]
     OP_LITL = 0x8000
     OP_LITH = 0xa000
     OP_RJP = 0xe000
@@ -29,6 +32,28 @@ class Dcpu:
     DST_R = 0x1
     DST_PC = 0x2
     DST_MEM = 0x3
+    ALU_T = 0
+    ALU_N = 1
+    ALU_R = 2
+    ALU_MEMT = 3
+    ALU_ADD = 4
+    ALU_SUB = 5
+    ALU_NOP = 6
+    ALU_AND = 7
+    ALU_OR = 8
+    ALU_XOR = 9
+    ALU_LTS = 10
+    ALU_LT = 11
+    ALU_SR = 12
+    ALU_SRW = 13
+    ALU_SL = 14
+    ALU_SLW = 15
+    ALU_JZ = 16
+    ALU_JNZ = 17
+    ALU_CARRY = 18
+    ALU_INV = 19
+    ALU_MULL = 20
+    ALU_MULH = 21
 
     def __init__(self, mif: DcpuMemoryIf):
         self._mif = mif
@@ -41,7 +66,7 @@ class Dcpu:
 
     def dsp_op(self, dspmod):
         assert dspmod in [self.DSP_INC, self.DSP_DEC], f"Invalid dspmod {dspmod}"
-        if dspmod == self.DSP_PLUS:
+        if dspmod == self.DSP_INC:
             self.dsp = (self.dsp + 1) % self.DS_SIZE
         elif dspmod == self.DSP_MINUS:
             self.dsp = (self.dsp - 1) % self.DS_SIZE
@@ -63,38 +88,99 @@ class Dcpu:
         return self.rs[self.rsp]
 
     def call(self):
-        # self.rpush(self._pc + 1)
         self.rsp_op(self.RSP_INC)
         self.rs[self.rsp] = (self._pc + 1) & 0xffff
         self._pc = self._ir & 0x7fff
+        print(f"call {self._pc}")
 
     def litl(self):
         self.dsp_op(self.DSP_INC)
         self.ds[self.dsp] = self._ir & ~0xe000
+        print(f"litl {self.ds[self.dsp]:x}")
 
     def lith(self):
         self.ds[self.dsp] = (self.ds[self.dsp] & 0xff) | ((self._ir & 0xff) << 8)
         if (self._ir & 0x100):
-            self.cpu_return()
-
-    def cpu_return(self):
-        self._pc = self.r()
-        self.rsp_op(self.RSP_DEC)
+            # return bit set
+            self._pc = self.r()
+            print(f"lith {self._ir & 0xff:x} [ret]")
+        else:
+            print(f"lith {self._ir & 0xff:x}")
 
     def rjp(self):
         cond = self._ir & 0x1c00
+        offs = self._ir & 0x3ff
+        if (offs > 0x1ff):
+            offs = offs - 0x400
         condition  = ((cond == self.RJP_COND_ALWAYS))
         condition |= ((cond == self.RJP_COND_ZERO) and (self.t() == 0))
         condition |= ((cond == self.RJP_COND_NOTZERO) and (self.t() != 0))
         condition |= ((cond == self.RJP_COND_NEGATIVE) and (self.t() & 0x8000))
         condition |= ((cond == self.RJP_COND_NOTNEGATIVE) and not (self.t() & 0x8000))
+
+        if cond == self.RJP_COND_ALWAYS: print(f"rj {self._pc + offs}")
+        elif cond == self.RJP_COND_ZERO: print(f"rj.z {self._pc + offs}")
+        elif cond == self.RJP_COND_NOTZERO: print(f"rj.nz {self._pc + offs}")
+        elif cond == self.RJP_COND_NEGATIVE: print(f"rj.n {self._pc + offs}")
+        elif cond == self.RJP_COND_NOT_NEGATIVE: print(f"rj.nn {self._pc + offs}")
+
         if condition:
-            offs = self._ir & 0x3ff
-            if (offs > 0x1ff): offs = offs - 0x200
-            self._pc = self._pc % 0x10000
+            self._pc = (self._pc + offs) & 0xffff
 
     def alu_op(self, op):
-        pass
+        if op == self.ALU_T:
+            return self.t()
+        elif op == self.ALU_N:
+            return self.n()
+        elif op == self.ALU_R:
+            return self.r()
+        elif op == self.ALU_MEMT:
+            return self._mif.read(self.t())
+        elif op == self.ALU_ADD:
+            sum = self.n() + self.t()
+            self.carry = 1 if sum > 0xffff else 0
+            return sum & 0xffff
+        elif op == self.ALU_SUB:
+            res = self.n() - self.t()
+            self.carry = 1 if res < 0 else 0
+            return res & 0xffff
+        elif op == self.ALU_NOP:
+            return self.t()
+        elif op == self.ALU_AND:
+            return (self.n() & self.t()) & 0xffff
+        elif op == self.ALU_OR:
+            return (self.n() | self.t()) & 0xffff
+        elif op == self.ALU_XOR:
+            return (self.n() ^ self.t()) & 0xffff
+        elif op == self.ALU_LTS:
+            print("TODO: signed comparison")
+            return self.n() < self.t() # TODO signed!
+        elif op == self.ALU_LT:
+            return self.n() < self.t()
+        elif op == self.ALU_SR:
+            return self.t() >> 1
+        elif op == self.ALU_SRW:
+            return self.t() >> 8
+        elif op == self.ALU_SL:
+            return self.t() << 1
+        elif op == self.ALU_SLW:
+            return self.t() << 8
+        elif op == self.ALU_JZ:
+            return self.n() if self.t() == 0 else (self._pc+1) & 0xffff
+        elif op == self.ALU_JNZ:
+            return self.n() if self.t() != 0 else (self._pc+1) & 0xffff
+        elif op == self.ALU_CARRY:
+            return self.carry
+        elif op == self.ALU_INV:
+            return ~self.t()
+        elif op == self.ALU_MULL:
+            return (self.n() * self.t()) & 0xffff
+        elif op == self.ALU_MULH:
+            return ((self.n() * self.t()) >> 16) & 0xffff
+        else:
+            assert False, f"Invalid alu op {op}"
+
+
 
     def alu(self):
         rsp_op = self._ir & 0x3
@@ -102,6 +188,8 @@ class Dcpu:
         dst = (self._ir >> 4) & 0x3
         ret = (self._ir >> 6) & 0x1
         op = (self._ir >> 7) & 0x1f
+        sys.stdout.print(f"a:{self.ALU_OP_MNEMONIC[op]}")
+
         result = self.alu_op(op)
         addr = self.t()
         self.dsp_op(dsp_op)
@@ -115,18 +203,21 @@ class Dcpu:
         elif dst == self.DST_MEM:
             self._mif.write(addr, result)
         if ret:
-            self.cpu_return()
+            self._pc = self.r()
 
     def reset(self):
         self._pc = 0
+        self.carry = 0
 
     def decode(self):
         if self._ir & 0x8000 == 0:
             self.call()
         elif self._ir & 0xe000 == 0x8000:
             self.litl()
+            self._pc = (self._pc + 1) & 0xffff
         elif self._ir & 0xe000 == 0xa000:
             self.lith()
+            self._pc = (self._pc + 1) & 0xffff
         elif self._ir & 0xe000 == 0xe000:
             self.rjp()
         elif self._ir & 0xe000 == 0xc000:
@@ -136,13 +227,16 @@ class Dcpu:
 
     def step(self):
         self._ir = self._mif.read(self._pc)
-        self._pc += 1
         self.decode()
 
 
 class Mif(DcpuMemoryIf):
-    def __init__(self):
+    def __init__(self, image_fn):
         self._mem = bytearray(0x20000)
+        with open(image_fn,"rb") as f:
+            buf = f.read()
+        for i in range(len(buf)):
+            self._mem[i] = buf[i]
 
     def read(self, wordaddr):
         return (self._mem[wordaddr*2] << 8) | self._mem[wordaddr*2+1]
@@ -151,8 +245,13 @@ class Mif(DcpuMemoryIf):
        self._mem[wordaddr*2] = (word >> 8) & 0xff
        self._mem[wordaddr*2+1] = word & 0xff
 
-mif = Mif()
+def main():
+    mif = Mif("prog.bin")
 
-cpu = Dcpu(mif)
-cpu.step()
+    cpu = Dcpu(mif)
+    cpu.step()
+    cpu.step()
+    cpu.step()
 
+if __name__ == "__main__":
+    main()
