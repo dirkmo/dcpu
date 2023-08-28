4f84492751050c10c6dbfc66684ed4237b7944d5
Dirk Moeller <moeller.dirk80@gmail.com>
2023-08-28 12:51:38 +0200

    dev

--- colorforth/convert.py
+++ colorforth/convert.py
@@ -45,21 +45,58 @@ for line in lines:
 
 print(tokens)
 
-D = {}
-here = 0
+class Token:
+    DEFINITION = 0
+    IMMEDIATE = 1
+    D = {}
+    Didx = 0
+    def __init__(self, tag):
+        self.tag = tag
+    
+    def addDefinition(name):
+        assert not name in Token.D, f"{name} already defined"
+        print(f"Definition {Token.Didx}: {name}")
+        Token.D[name] = Token.Didx
+        Token.Didx += 1
+
+    def generate(self):
+        ...
+
+class TokenDefinition(Token):
+    def __init__(self, name):
+        super().__init__(self.DEFINITION)
+        self.name = name
+        Token.addDefinition(name)
+
+    def generate(self):
+        pass
+
+class TokenImmediate(Token):
+    def __init__(self, s):
+        #super().__init__(self.imm
+        pass
+
+program = []
 
 for t in tokens:
-    if t[0] == ":" and len(t) > 1:
-        name = t[1:]
-        D[name] = here
-    elif t[0] == "^" and len(t) > 1:
-        # execute immediately
-        # means: assemble call word (not to dict)
-    elif t[0] == "#" and len(t) >1:
-        # execute immediately and compile result
-        # means: assemble call word (not to dict)
-        # and then compile result to dict as literal
-    elif t[0] == '"' and len(t) > 2:
-        # string literal
-    else:
-        # compile word
+    print(f"'{t}'")
+    if len(t) > 1:
+        if t[0] == ":":
+            program.append(TokenDefinition(t[1:]))
+        elif t[0] == "^":
+            # execute immediately
+            # means: assemble call word (not to dict)
+            pass
+        elif t[0] == "#":
+            # execute immediately and compile result
+            # means: assemble call word (not to dict)
+            # and then compile result to dict as literal
+            pass
+        elif t[0] == '"' and len(t) > 2:
+            # string literal
+            pass
+        else:
+            # compile word
+            pass
+    else: # empty line
+        pass
